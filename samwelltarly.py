2a53aaf9f77aee80e20708c45a7021a2e8301f9c
Daniel Maida <dfm2@cin.ufpe.br>
2017-08-28 18:30:33 -0300

    tokenization complete

--- samwelltarly.py
+++ samwelltarly.py
@@ -8,27 +8,25 @@ def parseHtml(html):
         script.extract()
     
     raw_content = soup.get_text()
-    return raw_content
+    return raw_content.encode('utf-8')
 
 def tokenizer(html):
     stopwords = []
-    
     with open("stopwords.txt", "r") as sw:
-        stopwords = sw.readlines()
-    
+        stopwords = sw.read()
     stopwords = stopwords.split()
-    page_content = parseHtml(html)
-    
-    for word in page_content(stopwords):
-        word.extract()
-    
-    return page_content              
+    page_content = parseHtml(html).split()
+    resultwords  = [word for word in page_content if word.lower() not in stopwords and len(word) > 3 and word.isalnum()]
+    result = ' '.join(resultwords)
+    return result              
 
 def main():
+   
    html = io.open("teste.html", encoding="utf-8")
-   html_content = parseHtml(html)
+   html_content = tokenizer(html)
    bag_of_words = html_content.split()
-   print(bag_of_words)
+   for word in bag_of_words:
+     print(word)
    
 
 if __name__ == '__main__':
