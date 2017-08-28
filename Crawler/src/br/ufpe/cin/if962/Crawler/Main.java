8ed72b1f8736f509da017b1dc07eb44baa62bbe7
Arthur Freitas <abf2@cin.ufpe.br>
2017-08-27 22:39:14 -0300

    Add first implementation of the web crawler

---
+++ Crawler/src/br/ufpe/cin/if962/Crawler/Main.java
@@ -0,0 +1,21 @@
+package br.ufpe.cin.if962.Crawler;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import br.ufpe.cin.if962.Config.Config;
+
+public class Main {
+	public static void main(String[] args) {
+		List<String> baseUrls = new ArrayList<String>();
+		baseUrls.add("https://www.saraiva.com.br/");
+		baseUrls.add("https://www.mercadolivre.com.br/");
+		
+		
+		Iterator<String> it = baseUrls.iterator();
+		while(it.hasNext()) {
+			new Thread(new SiteCrawlerRunnable(it.next(),Config.filePath, 100)).start();
+		}
+	}
+}
