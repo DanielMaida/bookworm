b44342540d9d62437397c2769476d4ca124883aa
jpms2 <jpms2@cin.ufpe.br>
2017-09-18 12:19:21 -0300

    relocating misadded file

--- Extractor/retrieve_data.rb
+++ Extractor/retrieve_data.rb
@@ -2,13 +2,161 @@
 #- cultura done
 #- saraiva done
 #- casas bahia done
-#- amazon
+#- amazon done
 #- mercado livre
-#- estante virtual
-#- americanas
-#- submarino
-#- magazine luiza
+#- estante virtual done
+#- americanas done
+#- submarino done
+#- magazine luiza done
 class Retrieve_data
+
+	def retrieve_magazine_luiza(path)
+		info = Info.new
+		info.author = ''
+		match_title = /h1 .*?>(.*?)</
+		match_author = /Autor .*?">(.*?)</
+		match_price = /te__t.*?>(.*?)</
+		match_isbn = /ISBN.*?">(.*?)</
+		file_lines = read_file path
+		file_lines.each do |line|
+			if match_title =~ line
+				title_publisher = (clean_output (match_title.match line)[1]).force_encoding(::Encoding::UTF_8)
+				info.title = (/(.*?) -.*/.match (title_publisher))[1]
+			    info.publisher = (/-(.*?)-/.match (title_publisher.reverse!))[1].reverse!.strip
+			end
+			if match_author =~ line && info.author == ''
+				info.author = (clean_output (match_author.match line)[1]).force_encoding(::Encoding::UTF_8).strip
+			end
+			if match_price =~ line
+				info.price_physical = (clean_output (match_price.match line)[1]).force_encoding(::Encoding::UTF_8)
+			end
+			if match_isbn =~ line
+				info.isbn = (clean_output (match_isbn.match line)[1]).force_encoding(::Encoding::UTF_8).gsub(/\s+/, "")
+			end
+		end		
+		puts info.title
+		puts info.author
+		puts info.publisher
+		puts info.price_physical
+		puts info.isbn
+	end
+
+
+	def retrieve_submarino(path)
+		info = Info.new
+		info.author = ''
+		match_title = /h1 i.*?>(.*?)</
+		match_author = /Autor.*?:"(.*?)"/
+		match_price = /<p class="sales-price.*?>(R\$ \d+,\d+)</
+		match_publisher = /Editora.*?:"(.*?)"/
+		match_isbn = /ISBN.*?:"(.*?)"/
+		match_pages = /P.*?ginas.*?:"(.*?)"/
+		file_lines = read_file path
+		file_lines.each do |line|
+			if match_title =~ line
+				info.title = (clean_output (match_title.match line)[1]).force_encoding(::Encoding::UTF_8)
+			end
+			if match_author =~ line && info.author == ''
+				info.author = (clean_output (match_author.match line)[1]).force_encoding(::Encoding::UTF_8)
+			end
+			if match_price =~ line
+				info.price_physical = (clean_output (match_price.match line)[1]).force_encoding(::Encoding::UTF_8)
+			end
+			if match_isbn =~ line
+				info.isbn = (clean_output (match_isbn.match line)[1]).force_encoding(::Encoding::UTF_8)
+			end
+			if match_pages =~ line
+				info.pages = (clean_output (match_pages.match line)[1]).force_encoding(::Encoding::UTF_8)
+			end
+			if match_publisher =~ line
+				info.publisher = (clean_output (match_publisher.match line)[1]).force_encoding(::Encoding::UTF_8)
+				if info.publisher[0] == ' '
+					info.publisher = info.publisher[1..-1]
+				end
+			end
+		end		
+		puts info.title
+		puts info.author
+		puts info.price_physical
+		puts info.publisher
+		puts info.pages
+		puts info.isbn
+	end
+
+
+	def retrieve_americanas(path)
+		info = Info.new
+		info.author = ''
+		match_title = /<td.*?>T.*?tulo.*?">(.*?)</
+		match_subtitle = /<td.*?>Subt.*?">(.*?)</
+		match_author = /<td.*?>Autor.*?">(.*?)</
+		match_price = /<p class="sales-price.*?>(R\$ \d+,\d+)</
+		match_publisher = /<td.*?>Editora.*?">(.*?)</
+		match_isbn = /<td.*?>ISBN.*?">(.*?)</
+		match_pages = /<td.*?>P.*?ginas.*?">(.*?)</
+		file_lines = read_file path
+		file_lines.each do |line|
+			if match_title =~ line
+				if match_subtitle =~ line
+					info.title = "#{((clean_output (match_title.match line)[1]).force_encoding(::Encoding::UTF_8))}: #{((clean_output (match_subtitle.match line)[1]).force_encoding(::Encoding::UTF_8))}"
+				else
+					info.title = (clean_output (match_title.match line)[1]).force_encoding(::Encoding::UTF_8)
+				end
+			end
+			if match_author =~ line && info.author == ''
+				info.author = (clean_output (match_author.match line)[1]).force_encoding(::Encoding::UTF_8)
+			end
+			if match_price =~ line
+				info.price_physical = (clean_output (match_price.match line)[1]).force_encoding(::Encoding::UTF_8)
+			end
+			if match_isbn =~ line
+				info.isbn = (clean_output (match_isbn.match line)[1]).force_encoding(::Encoding::UTF_8)
+			end
+			if match_pages =~ line
+				info.pages = (clean_output (match_pages.match line)[1]).force_encoding(::Encoding::UTF_8)
+			end
+			if match_publisher =~ line
+				info.publisher = (clean_output (match_publisher.match line)[1]).force_encoding(::Encoding::UTF_8)
+				if info.publisher[0] == ' '
+					info.publisher = info.publisher[1..-1]
+				end
+			end
+		end		
+		info
+	end
+
+
+	def retrieve_estante_virtual(path)
+		info = Info.new
+		info.author = ''
+		match_title = /h1 class="busca-title.*?>(.*)</
+		match_author = /h2 class="busca-author.*?>.*?>(.*)<\/a/
+		match_price = /<li class="price-book">(.*)</
+		match_publisher = /Editora:<\/strong>(.*)</
+		filled_p = false
+		filled_pu = false
+		file_lines = read_file path
+		file_lines.each do |line|
+			if match_title =~ line
+				info.title = (clean_output (match_title.match line)[1]).force_encoding(::Encoding::UTF_8)
+			end
+			if match_author =~ line && info.author == ''
+				info.author = (clean_output (match_author.match line)[1]).force_encoding(::Encoding::UTF_8)
+			end
+			if match_price =~ line && !filled_p
+				info.price_physical = (clean_output (match_price.match line)[1]).force_encoding(::Encoding::UTF_8)
+				filled_p = true
+			end
+			if match_publisher =~ line && !filled_pu
+				info.publisher = (clean_output (match_publisher.match line)[1]).force_encoding(::Encoding::UTF_8)
+				filled_pu = true
+				if info.publisher[0] == ' '
+					info.publisher = info.publisher[1..-1]
+				end
+			end
+		end		
+		info
+	end
 
 	def retrieve_amazon(path)
 		info = Info.new
@@ -35,10 +183,7 @@ class Retrieve_data
 				end
 			end
 		end		
-		puts info.title
-		puts info.author
-		puts info.price_physical
-		puts info.publisher
+		info
 	end
 
 
@@ -191,6 +336,6 @@ class Info
 	attr_accessor :author
 	attr_accessor :publisher
 	attr_accessor :price_physical
+	attr_accessor :pages
+	attr_accessor :isbn
 end
-
-Retrieve_data.new.retrieve_amazon "C:/Users/jpms2/Desktop/RI/amazon5.html"
