e32ee074f47a17e52f39c576be5dccd38f0a7ff9
Arthur Freitas <abf2@cin.ufpe.br>
2017-12-02 18:12:13 -0300

    Connect server response with query result

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var linkIdMap = require("../InvertedFile/linkIdMap.json");
 
+var fs = require('fs'),
+    readline = require('readline');   
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -15,9 +18,25 @@ app.get('/', function(req, res) {
 	var other = query.other;
 	if(author){
 		//TO DO: query using these fields
-		//acesso como linkIdMap['0999'];
 		//lembrar de transformar @ em " antes de mandar de volta, ou na tela msm sei lá
-		res.render('pages/index', {renderResultTable:true, query:query, resultados:[]});
+
+		//FAZER CHAMADA DO PYTHON PASSANDO OS PARAMS
+		/*const execSync = require('child_process').execSync;
+		code = execSync('node -v');*/
+
+		var resultados = [];
+
+		var rd = readline.createInterface({
+		    input: fs.createReadStream('queryResult.txt'),		    
+		    console: false
+		});
+
+		rd.on('line', function(line) {
+		    resultados.push({author:"",title:"",publisher:"",isbn:"",link:linkIdMap[line].replace(/@/g, "\"")});//espero que funcione =|
+		}).on('close', function() {
+		    	res.render('pages/index', {renderResultTable:true, query:query, resultados:resultados});
+		});
+		
 	}else{
 		res.render('pages/index', {renderResultTable:false, query:query});
 	}
